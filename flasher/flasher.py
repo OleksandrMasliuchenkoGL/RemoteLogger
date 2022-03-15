dcdc266ae58eb0f8be21b88368f14d4ab5660f67
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2022-03-15 10:14:20 +0200

    Add some more commands to flasher

--- flasher/flasher.py
+++ flasher/flasher.py
@@ -34,12 +34,13 @@ def sendRequest(ser, msgtype, data):
     data = ser.read(2)
     check(data, "No response from device")
 
-    msglen, msgtype = struct.unpack('BB', data)
-    trace("Response type={:02x} length={}".format(msgtype, msglen))
-    data = ser.read(msglen - 1)
+    resplen, resptype = struct.unpack('BB', data)
+    trace("Response type={:02x} length={}".format(resptype, resplen))
+    data = ser.read(resplen - 1)
     check(data, "Incorrect response from device")
 
-    trace("Received: " + "{:02x} {:02x} ".format(msglen, msgtype) + ' '.join('{:02x}'.format(x) for x in data))
+    trace("Received: " + "{:02x} {:02x} ".format(resplen, resptype) + ' '.join('{:02x}'.format(x) for x in data))
+    check(msgtype + 1 == resptype, "Incorrect response type")   # Looks like request and response type numbers are next to each other
 
     return data[:-1]
 
@@ -63,6 +64,14 @@ def setFlashType(ser):
     check(status[0] == 0, "Wrong status on select internal flash")
 
 
+def getMAC(ser):
+    print("Requesting device MAC address")
+    req = struct.pack("<IH", 0x01001570, 8) # Mac address is located at 0x01001570
+    resp = sendRequest(ser, 0x1f, req)
+    check(resp[0] == 0, "Wrong status on read MAC address")
+    return [x for x in resp[1:8]]
+
+
 def eraseFlash(ser):
     print("Erasing internal flash")
     resp = sendRequest(ser, 0x07, b'')
@@ -70,12 +79,23 @@ def eraseFlash(ser):
     check(status[0] == 0, "Wrong status on erase internal flash")
 
 
+def reset(ser):
+    print("Reset target device")
+    resp = sendRequest(ser, 0x14, b'')
+    status = struct.unpack("<B", resp)
+    check(status[0] == 0, "Wrong status on reset device")
+
+
 def main():
     ser = serial.Serial('COM5', baudrate=38400, timeout=1)
 
     getChipId(ser)
+    mac = getMAC(ser)
+    print("Device MAC address: " + ':'.join('{:02x}'.format(x) for x in mac))
+
     setFlashType(ser)
     eraseFlash(ser)
 
+    reset(ser)
 
 main()
